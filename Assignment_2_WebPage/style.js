6cfe9d3fa45bdee1bf7fb444fcb2177daba7285d
Mabindra Bahadur Moktan <lamamabindra@gmail.com>
2026-01-11 14:17:56 +1030

    check

--- Assignment_2_WebPage/style.js
+++ Assignment_2_WebPage/style.js
@@ -1,6 +1,30 @@
+// Change the Welcome Name
+function applyName() {
+    const input = document.getElementById("name-input").value;
+    document.getElementById("user-name").innerText = input || "Guest";
+}
+
+// Show Image and Description
 function showImage() {
-    const img = document.getElementById("artImage")
-    img.src = "b/Users/mabindramoktan/Desktop/buddha.jpg";
+    const img = document.getElementById("artImage");
+    // Change this to just "buddha.jpg" if the file is in your project folder
+    img.src = "buddha.jpg"; 
     img.style.display = "block";
-    document.getElementById("artDescription").textContent ="";
+    
+    document.getElementById("artDescription").innerText = "This is my first semester artwork of Buddha.";
+}
+
+// Toggle between Black and White Theme
+function toggleTheme() {
+    const body = document.body;
+    const btn = document.getElementById("theme-btn");
+
+    // Check if dark-mode class is already there
+    if (body.classList.contains("dark-mode")) {
+        body.classList.remove("dark-mode");
+        btn.innerText = "Switch to Dark Mode";
+    } else {
+        body.classList.add("dark-mode");
+        btn.innerText = "Switch to Light Mode";
+    }
 }
